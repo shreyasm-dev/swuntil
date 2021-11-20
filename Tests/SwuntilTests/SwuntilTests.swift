210a985026f391f484c646864bb0b214871fe256
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-11-20 14:12:48 -0600

    Write tests for until

--- Tests/SwuntilTests/SwuntilTests.swift
+++ Tests/SwuntilTests/SwuntilTests.swift
@@ -2,10 +2,15 @@ import XCTest
 @testable import Swuntil
 
 final class SwuntilTests: XCTestCase {
-  func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    XCTAssertEqual(Swuntil().text, "Hello, World!")
+  func testUntil() throws {
+    var i: Int = 0
+    
+    until {
+      return i == 5
+    } perform: {
+      i += 1
+    }
+
+    XCTAssertEqual(i, 5)
   }
 }
