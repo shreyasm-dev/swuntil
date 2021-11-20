e1bfae6223c7ba3b98d8578476525c88cc485241
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-11-20 14:01:51 -0600

    Fix formatting

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swuntil",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Swuntil",
-            targets: ["Swuntil"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Swuntil",
-            dependencies: []),
-        .testTarget(
-            name: "SwuntilTests",
-            dependencies: ["Swuntil"]),
-    ]
+  name: "Swuntil",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "Swuntil",
+      targets: ["Swuntil"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "Swuntil",
+      dependencies: []),
+    .testTarget(
+      name: "SwuntilTests",
+      dependencies: ["Swuntil"]),
+  ]
 )
--- Sources/Swuntil/Swuntil.swift
+++ Sources/Swuntil/Swuntil.swift
@@ -1,6 +1,6 @@
 public struct Swuntil {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
+  public private(set) var text = "Hello, World!"
+  
+  public init() {
+  }
 }
